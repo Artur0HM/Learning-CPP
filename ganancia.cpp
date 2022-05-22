c426bf2de466c96656ba3ceb073d5c743ef1f556
HURTADO MENDEZ Arturo <hurtadomendez90@gmail.com>
2022-05-22 00:11:42 -0500

    Calcula la ganacia de 15%

--- ganancia.cpp
+++ ganancia.cpp
@@ -1,4 +1,5 @@
-/* Un comerciante compra una cantidad de artículo a un costo dado. Determine el precio al cual debe venderlo si desea ganar el 15%. */
+/* Un comerciante compra una cantidad de artículo a un costo dado. 
+Determine el precio al cual debe venderlo si desea ganar el 15%. */
 
 #include <iostream>
 using namespace std;
@@ -13,7 +14,8 @@ int main (){
 
     miGanacia = (ganancia * precioDelProductoComprado) / 100;
 
-    cout << "Para ganar el " << ganancia << "% deberas de vender el producto a " << precioDelProductoComprado + miGanacia << endl;
+    cout << "Para ganar el " << ganancia << "% deberas de vender el producto a " 
+    << precioDelProductoComprado + miGanacia << endl;
     cout << "La ganancia de mi producto es de: " << miGanacia;
 
     return 0;
