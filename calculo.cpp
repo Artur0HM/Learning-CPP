2a731444d3763ef967b050ceecb293d9e1988c00
HURTADO MENDEZ Arturo <hurtadomendez90@gmail.com>
2022-05-26 23:24:44 -0500

    formula

---
+++ calculo.cpp
@@ -0,0 +1,27 @@
+/* Obtener (a-b)(a+b). */
+
+#include <iostream>
+using namespace std;
+int main () {
+
+    float ingrese_A;
+    float ingrese_B;
+    float resultado;
+
+    cout << "************FORMULA*******************" << endl;
+    cout << "*****       (a-b)(a+b)         *******" << endl;
+    cout << "**************************************" <<endl;
+
+
+    cout << "Ingrese A: ";
+    cin >> ingrese_A;
+
+    cout << "Ingrese B: ";
+    cin >> ingrese_B;
+
+    resultado = (ingrese_A - ingrese_B)*(ingrese_A + ingrese_B);
+
+    cout << "El resultado de (a-b)(a+b) es: " << resultado;
+
+    return 0;
+}
