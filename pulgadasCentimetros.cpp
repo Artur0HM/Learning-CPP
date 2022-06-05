46a1449989e412f9b3ef4fd90a21be68b103f24c
HURTADO MENDEZ Arturo <hurtadomendez90@gmail.com>
2022-06-05 00:41:52 -0500

    pulgadas a centimetros

---
+++ pulgadasCentimetros.cpp
@@ -0,0 +1,20 @@
+/* Permita introducir una medida expresada en centímetros la convierta en pulgadas (1pulgada = 2,54 centímetros) */
+
+#include <iostream>
+using namespace std;
+int main () {
+
+    int centimetros;
+    float pulgadaCentimetros = 2.54;
+    float pulgadas;
+
+    cout << "Ingresa una distancia en centimetros: ";
+    cin >> centimetros;
+
+    pulgadas = centimetros * pulgadaCentimetros;
+
+    cout << "La distancia ingresada es de: " << centimetros << " centimetros" << endl;
+    cout << "La distancia en pulgadas es de: " << pulgadas;
+
+    return 0;
+}
